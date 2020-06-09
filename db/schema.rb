355f7f6bbaa52065693adc2ad1b46d98a36ca21d
Hitoshi-unten <untenht@gmail.com>
2020-06-09 14:08:59 +0000

    勤怠８章8.4.1終了

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20200609125230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.boolean "admin", default: false
     t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
